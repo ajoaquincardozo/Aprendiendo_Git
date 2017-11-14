4bc9a49f50e02d85b981df65aeb6073c7702eb46
ajoaquincardozo <a.joaquincardozo@gmail.com>
2017-11-13 21:48:57 -0300

    Segundo conflicto por GIT

--- saludo.py
+++ saludo.py
@@ -4,4 +4,5 @@ def saludar():
 	print("Esta es una modificacion en la rama B")
 	print("Se modifica desde GIT para generar conflicto")
 	print("Se aniade cambios desde github")
+	print("Segundo conflicto pero desde GIT")
 saludar()
