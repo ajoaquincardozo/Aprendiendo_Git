cc039811ad3dbed9265b8852781aaac0598c0f64
alexandercardozo <33642644+alexandercardozo@users.noreply.github.com>
2017-11-13 21:47:48 -0300

    Segundo Conflicto

--- saludo.py
+++ saludo.py
@@ -4,4 +4,5 @@ def saludar():
 	print("Esta es una modificacion en la rama B")
 	print("Se modifica desde GIT para generar conflicto")
 	print("Se aniade cambios desde github")
+	print("Se modifica por segundad vez")
 saludar()
