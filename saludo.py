406d0d220365c08b83adc69ecddf2ff197c195d7
alexandercardozo <33642644+alexandercardozo@users.noreply.github.com>
2017-11-13 21:29:28 -0300

    Se modifica saludo.py para generar conflicto

--- saludo.py
+++ saludo.py
@@ -2,4 +2,5 @@ def saludar():
 	print("hola mundo!")
 	print("Esta es una modificacion en la rama A")
 	print("Esta es una modificacion en la rama B")
+	print("Se aniade cambios desde github")
 saludar()
